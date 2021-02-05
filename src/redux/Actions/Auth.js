3ffdb1fd2b10a20b07717c481c2d158f4423636e
aliraza0001 <aliarain214@gmail.com>
2021-02-06 01:57:57 +0500

    firestor and auth

--- src/redux/Actions/Auth.js
+++ src/redux/Actions/Auth.js
@@ -3,25 +3,39 @@ import * as Auth from '../../services/Auth';
 
 export const Login = (email, password) => {
   return async (dispatch) => {
+    dispatch({type: authState, payload: {loading: true}});
     try {
-      const response = await Auth.signInWithEmailAndPassword(email, password);
-
-      console.log('response---> ', response);
+      const {user} = await Auth.signInWithEmailAndPassword(email, password);
+      if (user?.uid) {
+        const userData = await Auth.getUser(uid);
+        console.log('userData', userData);
+        dispatch({
+          type: authState,
+          payload: {isLogin: true, loading: false, user: userData},
+        });
+      }
     } catch (error) {
       console.log('error---> ', error);
+      dispatch({type: authState, payload: {loading: false}});
     }
   };
 };
 export const Register = (email, password) => {
   return async (dispatch) => {
+    dispatch({type: authState, payload: {loading: true}});
     try {
-      const response = await Auth.createUserWithEmailAndPassword(
-        email,
-        password,
-      );
-      console.log('response---> ', response);
+      const {user} = await Auth.createUserWithEmailAndPassword(email, password);
+      if (user?.uid) {
+        const data = {name: 'Ali', email, Address: '14525'};
+        const userData = await Auth.createUser(user.uid, data);
+        console.log('userData', userData);
+        dispatch({type: authState, payload: {login: true, loading: false}});
+      }
+
+      console.log(email, password, 'response---> ', user);
     } catch (error) {
       console.log('error---> ', error);
+      dispatch({type: authState, payload: {loading: false}});
     }
   };
 };
@@ -34,6 +48,39 @@ export const SignOut = () => {
     } catch (error) {
       console.log('error---> ', error);
       dispatch({type: Logout});
+    }
+  };
+};
+
+export const getUsers = () => {
+  return async (dispatch) => {
+    dispatch({type: authState, payload: {loading: true}});
+    try {
+      const response = await Auth.getUsers();
+      console.log('response', response);
+      if (response) {
+        dispatch({type: authState, payload: {loading: false, users: response}});
+      }
+    } catch (error) {
+      console.log('error---> ', error);
+      dispatch({type: authState, payload: {loading: false}});
+    }
+  };
+};
+
+export const getCurrentUser = (uid) => {
+  return async (dispatch) => {
+    dispatch({type: authState, payload: {loading: true}});
+    try {
+      const userData = await Auth.getUser(uid);
+      console.log('userData', userData);
+      dispatch({
+        type: authState,
+        payload: {isLogin: true, loading: false, user: userData},
+      });
+    } catch (error) {
+      console.log('error---> ', error);
+      dispatch({type: authState, payload: {loading: false}});
     }
   };
 };
--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,4 +1,5 @@
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 // *** Auth API ***
 export const createUserWithEmailAndPassword = (email, password) =>
   auth().createUserWithEmailAndPassword(email, password);
@@ -6,8 +7,12 @@ export const createUserWithEmailAndPassword = (email, password) =>
 export const signInWithEmailAndPassword = (email, password) =>
   auth().signInWithEmailAndPassword(email, password);
 
-export const createUser = () => {};
+export const createUser = (uid, data) =>
+  firestore().collection('Users').doc(uid).set(data);
 
-export const getUser = () => {};
+export const getUser = async (uid) =>
+  firestore().collection('Users').doc(uid).get();
 
 export const logOut = () => auth().signOut();
+
+export const getUsers = () => firestore().collection('Users').get();
